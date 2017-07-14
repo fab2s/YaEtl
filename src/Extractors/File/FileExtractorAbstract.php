5a7ef63b7eaed3547c606dcb170aef7e4512a29e
fab2s <fabrice.de.stefanis@gmail.com>
2017-07-14 09:49:32 +0100

    Add File Extractor

---
+++ src/Extractors/File/FileExtractorAbstract.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of YaEtl.
+ *     (c) Fabrice de Stefanis / https://github.com/fab2s/YaEtl
+ * This source file is licensed under the MIT license which you will
+ * find in the LICENSE file or at https://opensource.org/licenses/MIT
+ */
+
+namespace fab2s\YaEtl\Extractors\File;
+
+use fab2s\NodalFlow\YaEtlException;
+use fab2s\YaEtl\Extractors\ExtractorAbstract;
+
+/**
+ * Class FileExtractorAbstract
+ */
+abstract class FileExtractorAbstract extends ExtractorAbstract
+{
+    /**
+     * @var string
+     */
+    protected $srcFile;
+
+    /**
+     * @var resource
+     */
+    protected $handle;
+
+    /**
+     * @param mixed|resource|string $input
+     *
+     * @throws YaEtlException
+     */
+    public function __construct($input)
+    {
+        if (is_resource($input)) {
+            $this->handle = $input;
+        } elseif (is_file($input)) {
+            $this->srcFile = $input;
+        } else {
+            throw new YaEtlException('Input is either not a resource or not a file');
+        }
+
+        parent::__construct();
+    }
+
+    /**
+     * @param mixed $param
+     *
+     * @return bool
+     */
+    public function extract($param = null)
+    {
+        if ($this->srcFile !== null) {
+            $this->handle = fopen($this->srcFile, 'rb');
+        }
+
+        if (!$this->handle | [!is_resource($this->handle)]) {
+            return false;
+        }
+
+        return rewind($this->handle);
+    }
+
+    /**
+     * release handle
+     *
+     * @return $this
+     */
+    public function releaseHandle()
+    {
+        if (is_resource($this->handle)) {
+            fclose($this->handle);
+        }
+
+        $this->handle = null;
+
+        return $this;
+    }
+}
---
+++ src/Extractors/File/LineExtractor.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of YaEtl.
+ *     (c) Fabrice de Stefanis / https://github.com/fab2s/YaEtl
+ * This source file is licensed under the MIT license which you will
+ * find in the LICENSE file or at https://opensource.org/licenses/MIT
+ */
+
+namespace fab2s\YaEtl\Extractors\File;
+
+/**
+ * Class LineExtractor
+ */
+class LineExtractor extends FileExtractorAbstract
+{
+    /**
+     * @param mixed $param
+     *
+     * @return \Generator
+     */
+    public function getTraversable($param)
+    {
+        if (!$this->extract($param)) {
+            return;
+        }
+
+        while (false !== ($line = fgets($this->handle))) {
+            yield $line;
+        }
+
+        $this->releaseHandle();
+    }
+}
